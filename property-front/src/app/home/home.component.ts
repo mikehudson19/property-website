9d9fa9fe2d6f8873804a93adcd5c70e26dc5600b
Michael Hudson <mikehudson19@live.co.uk>
2022-03-01 06:31:03 +0200

    WIP - implement new home page

--- property-front/src/app/home/home.component.ts
+++ property-front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IAdvert } from '@app/_models/IAdvert';
+import { AdvertService } from '@app/_services/advert.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  adverts: IAdvert[] = [];
+
+  constructor(private advertService: AdvertService) { }
 
   ngOnInit(): void {
+    this.advertService.getAllAdverts()
+      .subscribe(adverts => {
+        /** @TODO: Implement a feature to only return/display three featured adverts */
+        this.adverts = adverts;
+      })
   }
 
 }
