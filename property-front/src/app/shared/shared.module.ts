0698bd13a80a0d693e30704ab250e778465d90a6
Michael Hudson <mikehudson19@live.co.uk>
2022-03-01 15:16:29 +0200

    add the truncate text pipe to the shared module

--- property-front/src/app/shared/shared.module.ts
+++ property-front/src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TruncateTextPipe } from '@app/_helpers/truncate-text.pipe';
 
 
 
 @NgModule({
   declarations: [
-    SearchComponent
+    SearchComponent,
+    TruncateTextPipe
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule
   ],
   exports: [
-    SearchComponent
+    SearchComponent,
+    TruncateTextPipe
   ]
 })
 export class SharedModule { }
